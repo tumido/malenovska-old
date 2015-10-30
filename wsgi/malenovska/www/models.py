e0d8c3a5336f1d574b793204be1b3921c7991198
Tomas Coufal <tcoufal@redhat.com>
2015-10-30 01:21:25 +0100

    fix file saving

--- wsgi/malenovska/www/models.py
+++ wsgi/malenovska/www/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from redactor.fields import RedactorField
 from django.core.validators import MinValueValidator
+import os
+from django.conf import settings
 
 class Race(models.Model):
     name = models.CharField('Jméno rasy', max_length=200)
@@ -130,11 +132,29 @@ class Harmonogram(models.Model):
 class ExtraFiles(models.Model):
     name = models.CharField('Titulek', max_length=200)
     tooltip = models.CharField('Popisek', max_length=200)
-    filefield = models.FileField('Soubor')
+    filefield = models.FileField('Soubor', blank=True)
+    __original_file = None
 
     class Meta:
         verbose_name = 'Soubor'
         verbose_name_plural = 'Nahrané soubory'
 
+    def __init__(self, *args, **kwargs):
+        super(ExtraFiles, self).__init__(*args, **kwargs)
+        self.__original_file = self.filefield
+
     def __str__(self):
         return self.name
+
+    def save(self, force_insert=False, force_update=False):
+        if self.filefield != self.__original_file and self.__original_file != '':
+            f_path = os.path.join(settings.MEDIA_ROOT, str(self.__original_file))
+ 
+            try:
+                os.remove(f_path)
+                self.main_file.delete()
+            except:
+                pass
+
+        super(ExtraFiles, self).save(force_insert, force_update)
+        self.__original_file = self.filefield
