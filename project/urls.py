3633e410abea92bfffe6e87c5285eaca7b4d1da1
Tomas Coufal <coufal.tom@gmail.com>
2017-09-29 21:08:35 +0200

    Move apps to root

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,15 @@
+"""Malenovska URL paths config."""
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
-
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^redactor/', include('redactor.urls')),
+    url(r'^index/', include('news.urls')),
+    url(r'^register/', include('registration.urls')),
+    url(r'^world/', include('world.urls')),
+    url(r'^info/', include('info.urls')),
+    url(r'^rules/', include('rules.urls')),
+    url(r'^legends/', include('legends.urls')),
+    url(r'^', include('news.urls'))
 ]
