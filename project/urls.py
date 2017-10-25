94170b2796e539bdb7c29724f0f21459f48d92ba
Tomas Coufal <coufal.tom@gmail.com>
2017-10-25 22:06:00 +0200

    Add media url

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 """Malenovska URL paths config."""
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -13,4 +15,4 @@ urlpatterns = [
     url(r'^legends/', include('legends.urls')),
     url(r'^health', include('health_check.urls')),
     url(r'^', include('news.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
